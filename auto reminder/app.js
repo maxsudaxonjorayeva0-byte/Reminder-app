2cb4a7a323476f33b869e5c803ed876501b6a2ed
maxsudaxonjorayeva0-byte <maxsudaxonjorayeva0@gmail.com>
2026-05-26 16:37:16 +0500

    Add files via upload

---
+++ auto reminder/app.js
@@ -0,0 +1,38 @@
+// const addBtn = document.getElementById("addBtn")
+// const TaskInput = document.getElementById("TaskInput")
+// const TimeInput = document.getElementById("TimeInput")
+// const list = document.getElementById("list")
+
+// let reminders = [];
+
+// addBtn.addEventListener("click", () => {
+//     const task = TaskInput.value
+//     const time = TimeInput.value
+
+//     const remember = {
+//         task,
+//         time
+//     };
+
+
+// reminders.push(reminder);
+
+// renderReminders();
+// });
+
+// function renderReminders(){
+
+//     list.innerHTML = " ";
+
+//     reminders.forEach(reminder => {
+//         const div = document.createElement("div")
+
+//         div.className = "reminder"
+
+//         div.innerHTML =  ` <h3>${reminder.task}</h3>
+//         <p>${reminder.time}</p>`;
+
+//         list.appendChild(div);
+      
+//     });
+// }
